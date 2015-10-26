4e1ab49ac64cb3ff3e7c63ef36b498bd0c4cc4b9
Roberto <robertonovacid@gmail.com>
2015-10-26 23:28:53 +0100

    First commit Hola name

---
+++ encuestas/src/main/java/com/coderalia/encuestas/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package com.coderalia.encuestas.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping
+	public String home(Model model){
+		model.addAttribute("name", "Antonio Jose");
+		return "home";
+	}
+}
